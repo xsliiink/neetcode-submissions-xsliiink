5b74ecd7fc424ed1e47c51ed07bd4faa94e3e199
xsliiink <110228644+xsliiink@users.noreply.github.com>
2026-06-16 06:15:21 +0100

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    missingNumber(nums) {
+
+        let res = nums.length;
+
+        for(let i = 0; i < nums.length;i++){
+            res = res^ nums[i]^i;
+        }
+
+        return res;
+    }
+}
