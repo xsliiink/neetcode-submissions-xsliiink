04f507492c4f1eb665c3653d58beb4aeef0bc1d2
xsliiink <110228644+xsliiink@users.noreply.github.com>
2026-06-25 07:31:58 +0100

    Add: counting-bits - submission-3

---
+++ Data Structures & Algorithms/counting-bits/submission-3.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n) {
+        let res = [0];
+
+        for(let i = 1;i <= n;i++){
+            res[i] = res[i & (i - 1)] + 1;
+        }
+
+         return res;
+    }
+}
