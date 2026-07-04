3c6222fb526c6b75c19510ae1c1425bddd895b0b
xsliiink <110228644+xsliiink@users.noreply.github.com>
2026-07-04 07:02:37 +0100

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    numDecodings(s) {
+        //create an arr for storing the ways
+        const dp = new Array(s.length + 1).fill(0);
+        dp[0] = 1
+
+        //loop
+        for(let i = 1;i <= s.length;i++){
+            if(s[i - 1] !== "0"){
+                dp[i] += dp[i - 1]
+            }
+
+            if(i >= 2){
+                let twoDigit = parseInt(s.substring(i - 2,i))
+                if(twoDigit >= 10 && twoDigit <= 26){
+                    dp[i] += dp[i - 2]
+                }
+                
+            }
+        }
+
+        return dp[s.length]
+    }
+}
