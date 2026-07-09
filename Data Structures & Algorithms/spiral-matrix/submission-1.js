b13feacd10227f6e849833c57298038c9b2b5aae
xsliiink <110228644+xsliiink@users.noreply.github.com>
2026-07-09 06:41:23 +0100

    Add: spiral-matrix - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {number[]}
+     */
+    spiralOrder(matrix) {
+        let top = 0;
+        let right = matrix[0].length - 1;
+        let bottom = matrix.length - 1;
+        let left = 0;
+        let res =[];
+
+
+        
+        while(top <= bottom && left <= right){
+
+            //going right
+            for(let i = left;i <= right;i++){
+                res.push(matrix[top][i]);
+            }
+            top++
+
+            //going down
+            for(let i = top;i <= bottom;i++){
+                res.push(matrix[i][right])
+            }
+            right--;
+
+            //we need a check for going left
+            if(top <= bottom){
+                for(let i = right;i >= left;i--){
+                    res.push(matrix[bottom][i])
+                }
+                bottom--;
+            }
+            
+
+            //for going up we need a check
+            if(left <= right){
+                for(let i = bottom;i >= top;i--){
+                    res.push(matrix[i][left])
+                }
+                left++;
+            }
+        }
+        return res
+    }
+}
