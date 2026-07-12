0b1de364589587831dc8ee26473b2c3c6c5d5151
xsliiink <110228644+xsliiink@users.noreply.github.com>
2026-07-12 07:00:29 +0100

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+    coinChange(coins, amount) {
+        let dp = new Array(amount + 1).fill(amount + 1);
+
+        dp[0] = 0
+
+
+        //loop through the amount
+        for(let i = 0;i <= amount;i++){
+            for(let coin of coins){
+                if(i - coin >= 0 ){
+                    dp[i] = Math.min(dp[i],dp[i - coin] + 1)
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount]
+    }
+}
