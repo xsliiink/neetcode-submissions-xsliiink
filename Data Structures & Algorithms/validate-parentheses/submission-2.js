aa4ae43c4946ea581e53ddeb26372a886028fb13
xsliiink <110228644+xsliiink@users.noreply.github.com>
2026-07-22 06:58:48 +0100

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+        
+        const closeToOpen = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '[',
+        }
+
+
+        for(let char of s){
+            if(closeToOpen[char]){
+                const topElement = stack.length > 0 ? stack.pop() : '#'
+
+                if(topElement != closeToOpen[char]){
+                    return false;
+                }
+            }else{
+                stack.push(char)
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
