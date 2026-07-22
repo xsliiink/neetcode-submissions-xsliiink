f5db255bfc1f33d0520cf193e6740b1bc341a805
xsliiink <110228644+xsliiink@users.noreply.github.com>
2026-07-22 07:05:47 +0100

    Add: number-of-one-bits - submission-5

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-5.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n) {
+        let count = 0;
+
+        while(n !== 0){
+            n = n & (n - 1);
+            count++
+        }
+
+        return count;
+    }
+}
