0d0a2aa542330a966331a0a10ceed339e3fd334d
xsliiink <110228644+xsliiink@users.noreply.github.com>
2026-07-23 06:31:15 +0100

    Add: implement-prefix-tree - submission-5

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-5.js
@@ -0,0 +1,70 @@
+class TrieNode{
+    constructor(){
+        this.children = {}
+        this.isEnd = false;
+    }
+}
+
+class PrefixTree {
+    constructor() {
+        this.root = new TrieNode()
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {
+
+        let node = this.root
+
+        for(let char of word){
+            if(!node.children[char]){
+                node.children[char] = new TrieNode();
+            }
+
+            //moving the node
+            node = node.children[char];
+        }
+
+        node.isEnd = true;
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+
+        let node = this.root
+
+        for(let char of word){
+            if(!node.children[char]){
+                return false;
+            }
+
+            node = node.children[char]
+        }
+
+        return node.isEnd;
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {
+
+        let node = this.root;
+
+        for(let char of prefix){
+            if(!node.children[char]){
+                return false;
+            }
+
+            node = node.children[char]
+        }
+
+        return true;
+    }
+}
