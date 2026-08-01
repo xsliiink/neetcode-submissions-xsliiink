5590d29b7a17b5509d47a230e89f70b578106e86
xsliiink <110228644+xsliiink@users.noreply.github.com>
2026-08-01 06:56:38 +0100

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} text1
+     * @param {string} text2
+     * @return {number}
+     */
+    longestCommonSubsequence(text1, text2) {
+        //create an arr
+        let dp = Array.from({length: text1.length + 1},() => new Array(text2.length + 1).fill(0));
+        let M = text1.length;
+        let N = text2.length;
+
+        for(let i = 1;i <= M;i++){
+            for(let j = 1;j <= N;j++){
+                if(text1[i - 1] == text2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1])
+                }
+            }
+        }
+
+        return dp[M][N]
+    }
+}
